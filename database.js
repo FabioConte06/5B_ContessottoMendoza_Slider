080316ab0631365ba41f5d66319572e9eb394d98
Mendoza Enzo <mendozawayne@itis-molinari.eu>
2025-02-13 21:41:01 +0100

    fix database.js e index.html

--- database.js
+++ database.js
@@ -1,65 +1,54 @@
 const fs = require('fs');
 const mysql = require('mysql2');
-const { isNativeError } = require('util/types');
+
 let conf = JSON.parse(fs.readFileSync('conf.json'));
 conf.ssl = {
-    ca: fs.readFileSync(__dirname + './ca.pem')
-}
+    ca: fs.readFileSync(__dirname + '/ca.pem')  // Corretto il path
+};
+
 const connection = mysql.createConnection(conf);
 
-
-const executeQuery = (sql) => {
-   return new Promise((resolve, reject) => {      
-         connection.query(sql, function (err, result) {
+const executeQuery = (sql, params = []) => {
+    return new Promise((resolve, reject) => {
+        connection.query(sql, params, (err, result) => {
             if (err) {
-               console.error(err);
-               reject();     
-            }   
-            console.log('done');
-            resolve(result);         
-      });
-   })
-}
+                console.error(err);
+                reject(err);
+            } else {
+                resolve(result);
+            }
+        });
+    });
+};
 
 const database = {
-   createTable: () => {
-      return executeQuery(`
-      CREATE TABLE IF NOT EXISTS images
-         ( id INT PRIMARY KEY AUTO_INCREMENT,
-            name VARCHAR(255) NOT NULL)
-      `)
+    createTable: () => {
+        return executeQuery(`
+            CREATE TABLE IF NOT EXISTS images (
+                id INT PRIMARY KEY AUTO_INCREMENT,
+                url VARCHAR(255) NOT NULL
+            )
+        `);
     },
-   insert: (name) => {
-      let sql = "INSERT INTO images (name) VALUES ('$NAME')";
-      sql = sql.replace('$NAME', name);
-      return executeQuery(sql)
+    
+    insert: (url) => {
+        const sql = "INSERT INTO images (url) VALUES (?)";
+        return executeQuery(sql, [url]);
     },
-   select: () => {
-      const sql = `SELECT id, name FROM images`;
-      return executeQuery(sql);
+    
+    select: () => {
+        const sql = "SELECT id, url FROM images";
+        return executeQuery(sql);
     },
-   delete: (id) => {
-      let sql = `
-      DELETE FROM images
-      WHERE id = $ID
-      `;
-      return executeQuery(sql);
-    }, 
-   update: (todo) => {
-      let sql = `
-      UPDATE todo
-      SET completed=$COMPLETED
-      WHERE id=$ID
-         `;
-      sql = sql.replace("%ID", todo.id);
-      sql = sql.replace("%COMPLETED", todo.completed);
-      return executeQuery(sql); 
-   },
-   cancellaTutto: async () => {
-      return executeQuery("TRUNCATE TABLE images");
-   }
-
-
-}
+    
+    delete: (id) => {
+        const sql = "DELETE FROM images WHERE id = ?";
+        return executeQuery(sql, [id]);
+    },
+    
+    cancellaTutto: () => {
+        return executeQuery("TRUNCATE TABLE images");
+    }
+};
 
 module.exports = database;
