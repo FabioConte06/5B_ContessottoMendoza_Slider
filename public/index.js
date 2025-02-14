a47d159f2cde78d7a89d57023340f13bead8838e
Fabio Contessotto <contessottofabio@itis-molinari.eu>
2025-02-14 09:24:44 +0100

    middleware

---
+++ public/index.js
@@ -0,0 +1,42 @@
+const createMiddleware = () => {
+    return {
+        load: async () => {
+            const response = await fetch("/images");
+            const json = await response.json();
+            return json;
+        },
+        delete: async (id) => {
+            const response = await fetch("/delete/" + id, { method: "DELETE" });
+            const json = await response.json();
+            return json;
+        },
+        upload: async (inputFile) => {
+            const formData = new FormData();
+            formData.append("file", inputFile.files[0]);
+            const body = formData;
+            const fetchOptions = {
+                method: "POST",
+                body: body
+            };
+            try {
+                const res = await fetch("/upload", fetchOptions);
+                const data = await res.json();
+                console.log(data);
+            } catch (e) {
+                console.log(e);
+            }
+        }
+    }
+}
+const controller = async (middleware) => {
+    /* render */
+       const inputFile = document.querySelector('#file');
+       const button = document.querySelector("#button");
+       handleSubmit = async (event) => {
+       await middleware.upload(inputFile);
+        middleware.load().then(render);
+      }
+      button.onclick = handleSubmit;
+    }
+    
+    controller(createMiddleware());
